a3d0a9b66fbd417821e91f33491e9c8c0cd401ff
Fue Lee <fueclee.12@gmail.com>
2023-04-06 19:06:33 -0500

    button added to return to list page, functions as intended

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,14 +1,21 @@
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function Details() {
 
+    const history = useHistory();
     const details = useSelector(store => store.details)
+
+    const toList = () => {
+        history.push('/');
+    }
 
     return(
         <>
             <h1>{details.title}</h1>
             <img src={details.poster} />
             <p>{details.description}</p>
+            <button onClick={toList}>Back to List</button>
         </>
     )
 }
